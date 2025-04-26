a613fc458c2079da08063006119c89ddc0a1b437
evesquare <50450074+EveSquare@users.noreply.github.com>
2025-04-26 20:26:35 +0900

    Initial Commit

---
+++ FinderExtensionConvertGIF/FinderExtensionConvertGIFApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinderExtensionConvertGIFApp.swift
+//  FinderExtensionConvertGIF
+//
+//  Created by evesquare on 2025/04/26.
+//
+
+import SwiftUI
+
+@main
+struct FinderExtensionConvertGIFApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FinderExtensionConvertGIFTests/FinderExtensionConvertGIFTests.swift
@@ -0,0 +1,17 @@
+//
+//  FinderExtensionConvertGIFTests.swift
+//  FinderExtensionConvertGIFTests
+//
+//  Created by evesquare on 2025/04/26.
+//
+
+import Testing
+@testable import FinderExtensionConvertGIF
+
+struct FinderExtensionConvertGIFTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
